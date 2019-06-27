e9646a397e5291b832e578212bd10bb1acf2481a
Shola Ayeni <ayenisholah@yahoo.com>
2019-06-27 15:16:44 +0100

    Styled-components added to friend component

--- friends/src/components/Friend.jsx
+++ friends/src/components/Friend.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import styled from 'styled-components';
 
 const Friend = (props) => {
   return (
@@ -14,6 +14,24 @@ const Friend = (props) => {
   );
 }
 
+const StyledFriend = styled.div `
+  margin: 12px;
+  padding: 32px 16px;
+  border: 2px solid white;
+  border-radius: 5px;
 
+  .name {
+    margin-bottom: 8px;
+    font-weight: bold
+  }
+
+  .age {
+    margin-bottom: 8px;
+  }
+
+  .email {
+    margin-bottom: 8px;
+  }
+`;
 
 export default Friend;
