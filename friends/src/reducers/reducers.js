5490ae2eebe6a4d1df71056c3a7d1e00bd3909ba
Shola Ayeni <ayenisholah@yahoo.com>
2019-06-27 16:09:39 +0100

    errorMessageReducer function

--- friends/src/reducers/reducers.js
+++ friends/src/reducers/reducers.js
@@ -12,6 +12,7 @@ import {
   LOGGING_IN,
   LOGIN_SUCCESS
 } from '../actions/actionTypes';
+import { SlowBuffer } from 'buffer';
 
 export function friendsReducer(sliceOfState = [], action) {
   switch (action.type) {
@@ -21,6 +22,33 @@ export function friendsReducer(sliceOfState = [], action) {
       return action.payload;
     case DELETE_FRIEND_SUCCESS:
       return action.payload;
+    default:
+      return sliceOfState;
+  }
+}
+
+export function errorMessageReducer(sliceOfState = '', action) {
+  switch (action.type) {
+    case GETTING_FRIENDS_FAILURE:
+      return action.payload;
+    case GETTING_FRIENDS_SUCCESS:
+      return sliceOfState = '';
+
+    case ADD_FRIEND_FAILURE:
+      return action.payload;
+    case ADD_FRIEND_SUCCESS:
+      return sliceOfState = '';
+
+    case DELETE_FRIEND_FAILURE:
+      return action.payload;
+    case DELETE_FRIEND_SUCCESS:
+      return sliceOfState = '';
+
+    case LOGIN_FAILURE:
+      return action.payload;
+    case LOGIN_SUCCESS:
+      return sliceOfState;
+
     default:
       return sliceOfState;
   }
