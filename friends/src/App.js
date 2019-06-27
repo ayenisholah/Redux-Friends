a130c587a6ba012ad5be2ce52eb2b293f5db032c
Shola Ayeni <ayenisholah@yahoo.com>
2019-06-27 16:44:51 +0100

    App refactored

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Friends App</h1>
-    </div>
-  );
+
+
+class App extends React.Component {
+  
+  render() {
+    return (
+      <div className='App'>
+        {
+          this.props.errorMessage
+          && <h2 style={{color: 'red'}}>{ this.props.errorMessage }</h2>
+        }
+        {
+          this.props.requestPending
+          && <h2>Loading....</h2>
+        }
+      </div>
+    );
+  }
 }
+
 
 export default App;
